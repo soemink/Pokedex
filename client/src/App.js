ab6a41233d16852bc45e3eb94083d7f454c7924e
soemink <soeminkhant21@gmail.com>
2022-10-08 00:51:36 -0700

    test

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>testing1234</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
