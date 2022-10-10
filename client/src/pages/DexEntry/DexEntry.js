4ecf0baf7e99c2cec023e82e4c137861452abbf9
soemink <soeminkhant21@gmail.com>
2022-10-09 22:43:47 -0700

    Co-authored-by: djhspencer <djhspencer@users.noreply.github.com>

--- client/src/pages/DexEntry/DexEntry.js
+++ client/src/pages/DexEntry/DexEntry.js
@@ -1,12 +1,18 @@
+import React, { useState, useEffect } from "react";
+
 function DexEntry() {
   const Pokedex = require("pokeapi-js-wrapper")
   const P = new Pokedex.Pokedex()
-  P.getPokemonByName("eevee").then(function(response) {
-    console.log(response)
+  const [data, setData] = useState([])
+  const eevee = P.getPokemonByName("eevee").then(function(response) {
+    console.log(response);
+    setData(response["abilities"][0]["ability"]["name"])
   })
-
-  return (
-    <div>P</div>
+  
+   return (
+    <div>
+      <p>hello {data}</p>
+    </div>
   )
 }
 
