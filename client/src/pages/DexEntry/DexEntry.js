33138754a450fa14ff6346c1a949ff7dd59e1546
soemink <soeminkhant21@gmail.com>
2022-10-09 23:53:47 -0700

    fixed dex entry starting and added table to home

--- client/src/pages/DexEntry/DexEntry.js
+++ client/src/pages/DexEntry/DexEntry.js
@@ -3,15 +3,20 @@ import React, { useState, useEffect } from "react";
 function DexEntry() {
   const Pokedex = require("pokeapi-js-wrapper")
   const P = new Pokedex.Pokedex()
-  const [data, setData] = useState([])
-  const eevee = P.getPokemonByName("eevee").then(function(response) {
-    console.log(response);
-    setData(response["abilities"][0]["ability"]["name"])
-  })
+  const [data, setData] = useState("")
+
+  useEffect(() => {
+      P.getPokemonByName("eevee")
+        .then(response => {setData(response); console.log(response)})
+      }, [setData])
   
    return (
     <div>
-      <p>hello {data}</p>
+    {data !== "" &&
+      <div>
+      <p>hello <img src= {data.sprites.front_default}></img></p>
+    </div>
+    }
     </div>
   )
 }
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,52 @@
+import styled from "styled-components"
+
+const StyledTable = styled.table`
+    border: none;
+    border-collapse: collapse;
+    width: 100%;
+    box-shadow: 0 2px 10px rgb(255 255 255 / 20%);
+    margin-top: 5px;
+    margin-bottom: 5px;
+`
+
+const StyledTableCell = styled.td`
+    border: 1px solid rgb(255 255 255 / 20%);
+    text-align: center;
+    padding: 15px;
+    min-width: 100px;
+    max-width: calc(100% / 3) !important;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+`
+
+const StyledTableHeader = styled.thead`
+    background-color: black;
+    color: white;
+`
+
 function Home() {
 
+  const Pokedex = require("pokeapi-js-wrapper")
+  const P = new Pokedex.Pokedex()
+  // const [pokemon]
+
   return (
-    <div>Meow</div>
+    <div>
+    <StyledTable>
+      <StyledTableHeader>
+        <tr>
+          <StyledTableCell>#</StyledTableCell><StyledTableCell>Name</StyledTableCell><StyledTableCell>Image</StyledTableCell><StyledTableCell>Type</StyledTableCell>
+        </tr>
+      </StyledTableHeader>
+
+      <tbody>
+        {}
+      </tbody>
+    </StyledTable>
+    
+    
+    </div>
   )
 }
 
